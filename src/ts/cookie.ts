06e79a2320852de244286271b992ccfed33b3d35
Александр Актищев <aktishch@gmail.com>
2024-01-23 14:20:59 +0300

    data in cookie

--- src/ts/cookie.ts
+++ src/ts/cookie.ts
@@ -7,10 +7,15 @@ const getCookie = (): void => {
     if (!buner) return
 
     const id: string = buner.id
+    const value: string = String(buner.dataset.cookie)
+    const path: string = String(buner.dataset.path)
 
-    document.cookie = `cookie_${id}_active=1; path=/; max-age=${(7 * 24 * 60 * 60)}`
+    document.cookie = `cookie_${id}_${value}_active=1; path=${path}; max-age=${(7 * 24 * 60 * 60)}`
     dialog.close()
     buner.remove()
+
+    console.log(document.cookie);
+    
 
 }
 
